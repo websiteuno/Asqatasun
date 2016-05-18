3ef2b0fcc75a3d259a5a32e91517be0de0e480c8
koj <koj@asqatasun.org>
2016-05-18 02:56:47 +0200

    change view type of swagger

--- engine/asqatasun-web/src/main/java/org/asqatasun/rest/config/SwaggerConfig.java
+++ engine/asqatasun-web/src/main/java/org/asqatasun/rest/config/SwaggerConfig.java
@@ -35,7 +35,7 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
+		return new Docket(DocumentationType.SPRING_WEB)
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
