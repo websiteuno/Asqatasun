d570ca9994c73324dcc7ff79196f509c95aae52c
koj <koj@asqatasun.org>
2016-06-04 14:45:51 +0200

    add tgsi_ referential mysql scripts

---
+++ web-app/tgol-persistence/src/main/resources/sql/automated/common/MSDB-0.0.10__AddSIReferentials.sql
@@ -0,0 +1,8 @@
+INSERT IGNORE INTO `TGSI_REFERENTIAL` (`Code`, `Label`) VALUES
+('Aw22', 'Accessiweb 2.2') ;
+
+INSERT IGNORE INTO `TGSI_REFERENTIAL` (`Code`, `Label`) VALUES
+('Rgaa30', 'Rgaa 3.0');
+
+INSERT IGNORE INTO `TGSI_REFERENTIAL` (`Code`, `Label`) VALUES
+('Seo', 'Seo referential');
