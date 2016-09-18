69db3a487737ed88fc3aa62459878f480b4bc689
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:27 +0200

    Rgaa3.2016_1.4.6 from Rgaa3.0_1.4.5: new ID / new content
    ----> not enabled in database
    
    rule 1.4.6: new ID (1.4.6 instead of 1.4.5) and new content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010405.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010405Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule010405/*
    /documentation/en/90_Rules/rgaa3.0/01.Images/Rule-1-4-5.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010406.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010406.java
@@ -17,9 +17,17 @@
  *
  * Contact us by mail: asqatasun AT asqatasun DOT org
  */
+
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.ruleimplementation.AbstractDetectionPageRuleImplementation;
+import org.asqatasun.rules.elementselector.CaptchaElementSelector;
+import org.asqatasun.rules.elementselector.SimpleElementSelector;
+import static org.asqatasun.rules.keystore.AttributeStore.SRC_ATTR;
+import static org.asqatasun.rules.keystore.CssLikeQueryStore.EMBED_TYPE_IMG_NOT_IN_LINK_CSS_LIKE_QUERY;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_CAPTCHA_ALTERNATIVE_MSG;
 
 /**
  * Implementation of the rule 1.4.6 of the referential RGAA 3.2016
@@ -27,15 +35,23 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/01.Images/Rule-1-4-6.html">the rule 1.4.6 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-1-4-6">1.4.6 rule specification</a>
  *
- * @author
  */
-public class Rgaa32016Rule010406 extends AbstractNotTestedRuleImplementation {
+public class Rgaa32016Rule010406 extends AbstractDetectionPageRuleImplementation {
 
     /**
      * Default constructor
      */
-    public Rgaa32016Rule010406 () {
-        super();
+    public Rgaa32016Rule010406  () {
+        super(
+                new CaptchaElementSelector(
+                    new SimpleElementSelector(EMBED_TYPE_IMG_NOT_IN_LINK_CSS_LIKE_QUERY)),
+                // solution when at least one element is found
+                new ImmutablePair(TestSolution.NEED_MORE_INFO,CHECK_CAPTCHA_ALTERNATIVE_MSG),
+                // solution when no element is found
+                new ImmutablePair(TestSolution.NOT_APPLICABLE,""),
+                // evidence elements
+                SRC_ATTR
+            );
     }
 
 }
